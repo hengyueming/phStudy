c918567f55085fb8297ca97f63bf913db353eb5a
unknown <1987heng@gmail.com>
2020-10-22 16:02:47 -0700

    add more code' '

--- PyStudy.py
+++ PyStudy.py
@@ -4,3 +4,17 @@ x = 3
 y = 5
 
 print(x + y)
+
+answer = "you are the best"
+
+print(y , answer)
+
+if y > 0 :
+    print(answer + "+++the number is", y)
+    y -= 1
+else:
+    print(y + "the truth");
+
+while x > 0:
+    print("x going down :", x)
+    x -= 1;
